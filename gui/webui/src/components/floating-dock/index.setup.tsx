03b34651eec03e82e4ad854e3a783eb1d398814f
daief <1437931235@qq.com>
2026-09-01 00:06:10 +0800

    fix: adapt floating dock on narrow screens

--- gui/webui/src/components/floating-dock/index.setup.tsx
+++ gui/webui/src/components/floating-dock/index.setup.tsx
@@ -3,6 +3,7 @@ import { IconButton } from '@/components/button';
 import { useFloatingDockStore } from '@/store/floating-dock';
 import { VerticalAlignTopOutlined } from '@vicons/material';
 import { useI18n } from 'vue-i18n';
+import './style.css';
 
 const floatingDockStore = useFloatingDockStore();
 
@@ -17,14 +18,16 @@ export default __render(() => {
   const content = floatingDockStore.content;
 
   return (
-    <div class="fixed bottom-12 right-6 z-floating-dock flex max-h-76 flex-col items-center rounded border border-outline-variant bg-surface-container-lowest p-2 shadow-sm">
-      <IconButton title={t('common.goToTop')} onClick={scrollToTop}>
-        <VerticalAlignTopOutlined />
-      </IconButton>
-      {content ? (
-        <div class="my-1 h-px w-6 shrink-0 bg-outline-variant"></div>
-      ) : null}
-      {content?.()}
+    <div class="floating-dock fixed bottom-12 right-6 z-floating-dock flex max-h-76 flex-col items-center rounded border border-outline-variant bg-surface-container-lowest p-2 shadow-sm">
+      <div class="floating-dock__items flex flex-col items-center">
+        <IconButton title={t('common.goToTop')} onClick={scrollToTop}>
+          <VerticalAlignTopOutlined />
+        </IconButton>
+        {content ? (
+          <div class="my-1 h-px w-6 shrink-0 bg-outline-variant"></div>
+        ) : null}
+        {content?.()}
+      </div>
     </div>
   );
 });
